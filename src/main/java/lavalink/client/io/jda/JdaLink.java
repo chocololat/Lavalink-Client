fcdf238d68a02df78402682b2c97c2f751409278
Alice <atamatori56@gmail.com>
2022-09-13 18:31:10 +0200

    updated jda5 alpha19

--- src/main/java/lavalink/client/io/jda/JdaLink.java
+++ src/main/java/lavalink/client/io/jda/JdaLink.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
